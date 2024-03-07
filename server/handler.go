1a73c4b9af7d3f33aeeb755fc3ea85083e9ba867
AliAkhmetov <ali.akhmetov.amo@gmail.com>
2024-03-07 08:52:05 +0500

    CORSMiddleware

--- server/handler.go
+++ server/handler.go
@@ -24,6 +24,22 @@ type errorResponse struct {
 	Message string `json:"message"`
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 // NewHandler create Handler struct with repos parameter
 func NewHandler(repos *repository.Repository) *Handler {
 	return &Handler{repos: repos}
@@ -37,6 +53,8 @@ func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
 
 	//	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	router.Use(CORSMiddleware())
 
 	auth := router.Group("/auth")
 	{
